c325ad696e092fb859b3f014c5e2170022dabab5
Xuanwo <Xuanwo@users.noreply.github.com>
2023-04-03 12:54:03 +0000

    deploy: 29b483d0e97bd476ecd84578da4beccb2562935b

--- docs/rust/opendal/docs/internals/sidebar-items.js
+++ docs/rust/opendal/docs/internals/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["accessor","The internal implement details of `Accessor`."],["layer","The internal implement details of `Layer`."]]};
+window.SIDEBAR_ITEMS = {"mod":[["accessor","The internal implementation details of `Accessor`."],["layer","The internal implementation details of `Layer`."]]};
